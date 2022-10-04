23847bba71d072b14cb506ba03a8f98df01fa599
Dmitriy Baranov <amareelez@gmail.com>
2022-10-04 23:16:44 +0400

    Treat compilation warnings as errors

--- tools/BuildFlags.cmake
+++ tools/BuildFlags.cmake
@@ -1,5 +1,5 @@
 if (CMAKE_COMPILER_IS_GNUCC OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wpedantic -g")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wpedantic -Werror -g")
 else()
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -g")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Werror -g")
 endif()
